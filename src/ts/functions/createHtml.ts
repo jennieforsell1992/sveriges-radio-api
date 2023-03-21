1ab733fb16e77c89b63bc523989b4cb0a38e4fc6
Jennie <forselljennie@gmail.com>
2023-03-21 11:30:34 +0100

    changed appendChild to createhtml function

--- src/ts/functions/createHtml.ts
+++ src/ts/functions/createHtml.ts
@@ -1,20 +1,26 @@
 import { getPodcasts } from "../service/api";
-import { IProgram } from "../models/IProgram";
+import type { IProgram } from "../models/IProgram";
 
 const podCastContainer: HTMLElement = document.querySelector(
   ".section__podlist-pods"
 ) as HTMLElement;
-const innerArticle = document.createElement("article");
-const textDiv = document.createElement("div");
 
-function createInnerArticle() {
+function createInnerArticlePodcast() {
+  const innerArticle = document.createElement("article");
   innerArticle.setAttribute("class", "section__article-innerarticle");
-  podCastContainer.appendChild(innerArticle);
   return innerArticle;
 }
+function createImgPodcast(podcast: IProgram) {
+  const imgPodcast = document.createElement("IMG");
+  imgPodcast.setAttribute("src", podcast.socialimage);
+  imgPodcast.setAttribute("alt", "omslagsbild på" + " " + podcast.name);
+  imgPodcast.setAttribute("width", "100");
+  imgPodcast.setAttribute("height", "100");
+  return imgPodcast;
+}
 function createTextDivPodcast() {
+  const textDiv = document.createElement("div");
   textDiv.setAttribute("class", "section__article-div");
-  innerArticle.appendChild(textDiv);
   return textDiv;
 }
 
@@ -23,42 +29,43 @@ function createLinkPodcast(podcast: IProgram) {
   const linkText = document.createTextNode("Lyssna här");
   linkPodcast.setAttribute("href", podcast.programurl);
   linkPodcast.appendChild(linkText);
-  textDiv.appendChild(linkPodcast);
+  return linkPodcast;
 }
 
-function createImgPodcast(podcast: IProgram) {
-  const imgPodcast = document.createElement("IMG");
-  imgPodcast.setAttribute("src", podcast.socialimage);
-  imgPodcast.setAttribute("alt", "omslagsbild på" + " " + podcast.name);
-  imgPodcast.setAttribute("width", "100");
-  imgPodcast.setAttribute("height", "100");
-  innerArticle.appendChild(imgPodcast);
-}
-
-//ändra funktionsnamn
 function createDescPodcast(podcast: IProgram) {
   const descPodcast = document.createElement("p");
   const descText = document.createTextNode(podcast.description);
   descPodcast.appendChild(descText);
-  textDiv.appendChild(descPodcast);
+  return descPodcast;
 }
 
 function createHeaderPodcast(podcast: IProgram) {
   const headerPodcast = document.createElement("h2");
-  //ändra variabelnamn
-  const programName = document.createTextNode(podcast.name);
-  headerPodcast.appendChild(programName);
-  textDiv.appendChild(headerPodcast);
+  const podcastName = document.createTextNode(podcast.name);
+  headerPodcast.appendChild(podcastName);
+  return headerPodcast;
 }
 export async function createHtml() {
   const podCasts = await getPodcasts();
+  console.log(podCasts);
   podCasts.programs.forEach((podcast: IProgram) => {
-    createInnerArticle();
-    createTextDivPodcast();
-    createImgPodcast(podcast);
-    createLinkPodcast(podcast);
-    createHeaderPodcast(podcast);
-    createDescPodcast(podcast);
+    const innerArticle = createInnerArticlePodcast();
+    podCastContainer.appendChild(innerArticle);
+
+    const imgPodcast = createImgPodcast(podcast);
+    innerArticle.appendChild(imgPodcast);
+
+    const textDiv = createTextDivPodcast();
+    innerArticle.appendChild(textDiv);
+
+    const headerPodcast = createHeaderPodcast(podcast);
+    textDiv.appendChild(headerPodcast);
+
+    const descPodcast = createDescPodcast(podcast);
+    textDiv.appendChild(descPodcast);
+
+    const linkPodcast = createLinkPodcast(podcast);
+    textDiv.appendChild(linkPodcast);
   });
 }
 
--- src/ts/service/api.ts
+++ src/ts/service/api.ts
@@ -1,4 +1,6 @@
-export async function getPodcasts() {
+import { IProgramResponse } from "../models/IProgramResponse";
+
+export async function getPodcasts(): Promise<IProgramResponse> {
   return await fetch(
     "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false"
   )
